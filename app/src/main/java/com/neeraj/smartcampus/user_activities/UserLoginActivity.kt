df1ee508b3f15f7d17a99a3ad315ec3dc601299b
Shruti Bhateja <shrutibhateja1803@gmail.com>
2022-04-23 11:05:18 +0530

    Updated UI

--- app/src/main/java/com/neeraj/smartcampus/user_activities/UserLoginActivity.kt
+++ app/src/main/java/com/neeraj/smartcampus/user_activities/UserLoginActivity.kt
@@ -1,12 +1,19 @@
 package com.neeraj.smartcampus.user_activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.neeraj.smartcampus.R
+import kotlinx.android.synthetic.main.activity_user_login.*
 
 class UserLoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_login)
+
+        getStartedSignUp.setOnClickListener {
+            startActivity(Intent(this, UserSignUpActivity::class.java))
+            finish()
+        }
     }
 }
